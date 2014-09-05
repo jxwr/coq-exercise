a97b16be57b2305e738fe658ec4c6c7c885b5c25
Jiao Xiang <jxwr.cn@gmail.com>
2014-09-05 23:02:14 +0800

    ex1

--- coq-ex1.v
+++ coq-ex1.v
@@ -15,3 +15,31 @@ Check fourfold.
 
 (* global *)
 Parameter max_int : nat.
+
+(* section 定义块，同时也辅助定义函数 *)
+Section bin_def.
+  (* local *)
+  Variable a b : nat.
+  Definition c := a + b.
+End bin_def.
+
+Print c.
+
+Theorem imp_trans (p q r: Prop): 
+  (p -> q) -> (q -> r) -> p -> r.
+Proof.
+  intros.
+  apply H0.
+  apply H.
+  exact H1. (* or assumption *)
+Qed.
+
+Print imp_trans.
+
+Lemma l_35_36 :
+  forall n : nat, 7*5 < n -> 6*6 <= n.
+Proof.
+  intros.
+  exact H.
+Qed.
+
